ae2a2a55236156ade33ecef82034cabe99f1fc76
Sehrish Javed <sehrishjaved119@gmail.com>
2025-07-26 01:00:11 +0500

    added function.cpp
    introduction

---
+++ function.cpp
@@ -0,0 +1,14 @@
+// function
+#include<iostream>
+using namespace std;
+int firstfn(){
+	cout<<"Introduction to function!\n";
+	return 3;
+}//This part of function that we declared before main finction is called function definition.
+int main()
+{
+//	int value =firstfn();
+	cout<<firstfn()<<endl;//this is called function call/or function invoke
+//	cout<<"value:"<<value<<endl;
+	return 0;
+}
